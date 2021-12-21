610f14b9ea47b1377db7f6e9d9de69c9b075f386
pevdokim <95922789+pevdokim@users.noreply.github.com>
2021-12-21 10:36:48 -0500

    Update

--- tables2.py
+++ tables2.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import statsmodels.formula.api as sm
+from stargazer.stargazer import Stargazer
 from matplotlib import pyplot as plt
 
-# load the data
-df = pd.read_csv("data_ee.csv")
+df = pd.read_csv('data_ee.csv')
 
 # sort the data and create a variable keeping track of the partner's guess (row above or below, depending on condition)
+
 df = df.sort_values(by=["lon", "mturk", "private", "ses", "round", "gameperiod", "group", "type"]).reset_index()
 df["guess_prev"] = df.shift(1).guess
 df["guess_next"] = df.shift(-1).guess
 
-
 # define a function for computing belief accuracy
+
 def acc_compute(row):
     if row.mturk == 0:
         return 1-abs(row["guess"]-row["guess_prev"])
@@ -23,8 +25,16 @@ def acc_compute(row):
 # apply this function to every row
 df["acc"] = df.apply(acc_compute, axis=1)
 
-# create a variable indexing player role in both lab and mturk treatments
+# run the regressions
+reg1 = sm.ols(formula="acc ~ private", data=df[(df.mturk==0) & (df.type>1)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk==0) & (df.type>1)]["unique"]}, use_t=True)
+reg2 = sm.ols(formula="acc ~ private", data=df[(df.mturk == 0) & (df.type == 2)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk == 0) & (df.type == 2)]["unique"]}, use_t=True)
+reg3 = sm.ols(formula="acc ~ private", data=df[(df.mturk==0) & (df.type==3)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk==0) & (df.type==3)]["unique"]}, use_t=True)
+reg4 = sm.ols(formula="acc ~ private * gameperiod", data=df[(df.mturk==0) & (df.type>1)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk==0) & (df.type>1)]["unique"]}, use_t=True)
+reg5 = sm.ols(formula="acc ~ private", data=df[(df.mturk==1) & (df.lon==0)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk==1) & (df.lon==0)]["unique"]}, use_t=True)
+reg6 = sm.ols(formula="acc ~ private * gameperiod", data=df[(df.mturk==1) & (df.lon==0)]).fit(cov_type="cluster", cov_kwds={"groups": df[(df.mturk==1) & (df.lon==0)]["unique"]}, use_t=True)
 
+
+# print(Stargazer([reg1, reg2, reg3, reg4, reg5, reg6]).render_html())
 
 def labtype(row):
     if row["mturk"]==0:
@@ -32,13 +42,11 @@ def labtype(row):
     else:
         return 2
 
+df["labtype"]=df.apply(labtype, axis=1)
 
-df["labtype"] = df.apply(labtype, axis=1)
+df.info()
 
-# compute mean accuracy by player role
 acc_mean = df[df.labtype>1].groupby(["mturk", "lon", "private", "gameperiod", "labtype"]).acc.mean().reset_index()
-
-# make the plots
 
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(6, 6))
 
